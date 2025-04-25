5cef48fcf3c2e7902caf0c09b55e6ef60c4289ba
Zainab <zainabalsaffar03@gmail.com>
2025-04-26 00:48:20 +0300

    Step 2 complete: As a developer, I can test user registration endpoint (JUnit/Jupiter)

--- src/main/kotlin/com/nbk/coded/bankingService/user/userService.kt
+++ src/main/kotlin/com/nbk/coded/bankingService/user/userService.kt
@@ -22,16 +22,3 @@ class UserService(
 }
 
 
-
-//import org.springframework.stereotype.Service
-//
-//@Service
-//class UserService(val userRepo: UserRepository) {
-//    fun registerUser(username: String, password: String): User {
-//        val user = User(username = username, password = password)
-//        return userRepo.save(user)
-//    }
-//    fun getAllUsers(): List<User> {
-//        return userRepo.findAll()
-//    }
-//}
--- src/test/kotlin/com/nbk/coded/bankingService/BankingServiceApplicationTests.kt
+++ src/test/kotlin/com/nbk/coded/bankingService/BankingServiceApplicationTests.kt
@@ -1,13 +1,70 @@
 package com.nbk.coded.bankingService
 
+import com.nbk.coded.bankingService.user.UserRepository
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.*
+import org.springframework.security.crypto.password.PasswordEncoder
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import com.nbk.coded.bankingService.user.User
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
-class BankingServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class UserRegistrationTest {
+
+	@LocalServerPort
+	private var port: Int = 0
+
+	@Autowired
+	lateinit var restTemplate: TestRestTemplate
+
+	@Autowired
+	lateinit var userRepository: UserRepository
+
+	@Autowired
+	lateinit var passwordEncoder: PasswordEncoder
+
+	data class RegisterUserDTO(val username: String, val password: String)
+
+	companion object {
+		@JvmStatic
+		@BeforeAll
+		fun setUp(@Autowired userRepository: UserRepository, @Autowired passwordEncoder: PasswordEncoder) {
+			userRepository.deleteAll()
+
+			val testUser = User(
+				username = "coded",
+				password = passwordEncoder.encode("joincoded")
+			)
+			userRepository.save(testUser)
+		}
 	}
 
+	@Test
+	fun `test user registration`() {
+		val url = "http://localhost:$port/users/v1/register"
+
+		val headers = HttpHeaders().apply {
+			contentType = MediaType.APPLICATION_JSON
+		}
+
+		val body = RegisterUserDTO(username = "testuser123", password = "pass123")
+		val request = HttpEntity(body, headers)
+
+		val response = restTemplate.postForEntity(url, request, User::class.java)
+
+		assertEquals(HttpStatus.OK, response.statusCode)
+
+		assertNotNull(response.body?.id)
+
+		assertEquals("testuser123", response.body?.username)
+	}
 }
